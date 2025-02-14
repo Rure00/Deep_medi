5a221a12e965a6356e9f07e0df3db0ce1f759d18
rure00 <asnjkd@naver.com>
2025-02-14 22:30:23 +0900

    add: set Attribute state on HomeScreen.kt

--- app/src/main/java/com/rure/deepmedi/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/rure/deepmedi/presentation/home/HomeScreen.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -18,7 +20,14 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.rure.deepmedi.MainActivity
 import com.rure.deepmedi.R
+import com.rure.deepmedi.data.entity.AttributeTag
 import com.rure.deepmedi.presentation.MainViewModel
+import com.rure.deepmedi.presentation.model.Attribute
+import com.rure.deepmedi.presentation.model.BirthAttr
+import com.rure.deepmedi.presentation.model.BloodPressureAttr
+import com.rure.deepmedi.presentation.model.GenderAttr
+import com.rure.deepmedi.presentation.model.HeartRateAttr
+import com.rure.deepmedi.presentation.model.find
 import com.rure.deepmedi.ui.theme.Black
 import com.rure.deepmedi.ui.theme.Typography
 
@@ -30,6 +39,19 @@ fun HomeScreen(
 ) {
     val userData by mainViewModel.userDataState.collectAsState()
     val userAttribute by mainViewModel.userAttrState.collectAsState()
+
+    val gender by remember { derivedStateOf {
+        userAttribute.find(AttributeTag.Gender) as GenderAttr
+    } }
+    val birth by remember { derivedStateOf {
+        userAttribute.find(AttributeTag.Birth) as BirthAttr
+    } }
+    val heartRate by remember { derivedStateOf {
+        userAttribute.find(AttributeTag.HeartRate) as HeartRateAttr
+    } }
+    val bloodPressure by remember { derivedStateOf {
+        userAttribute.find(AttributeTag.BloodPressure) as BloodPressureAttr
+    } }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -43,7 +65,7 @@ fun HomeScreen(
         )
         Spacer(modifier = Modifier.height(12.dp))
         Text(
-            text = userAttribute[],
+            text = gender.value,
             style = Typography.titleMedium,
             color = Black
         )
