e615d145fedf55ccbbe61acc88a2e8aa7f2ae99f
rure00 <asnjkd@naver.com>
2025-02-15 11:56:24 +0900

    add: degree chip component

---
+++ app/src/main/java/com/rure/deepmedi/presentation/home/component/DegreeChip.kt
@@ -0,0 +1,37 @@
+package com.rure.deepmedi.presentation.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.rure.deepmedi.presentation.model.Degree
+import com.rure.deepmedi.ui.theme.Typography
+
+@Composable
+fun DegreeChip(
+    degree: Degree
+) {
+    Box(
+        modifier = Modifier
+            .wrapContentSize()
+            .clip(RoundedCornerShape(35.dp))
+            .background(color = degree.color)
+            .padding(horizontal = 22.dp, vertical = 8.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = degree.tag,
+            style = Typography.labelSmall,
+            color = degree.textColor,
+            textAlign = TextAlign.Center,
+        )
+    }
+}
--- app/src/main/java/com/rure/deepmedi/presentation/model/Degree.kt
+++ app/src/main/java/com/rure/deepmedi/presentation/model/Degree.kt
@@ -1,9 +1,23 @@
 package com.rure.deepmedi.presentation.model
 
-enum class Degree(val tag: String) {
-    Danger("위험"),
-    Warning("경고"),
-    Attention("주의"),
-    Normal("정상"),
-    Healthy("건강")
+import androidx.annotation.ColorRes
+import androidx.compose.ui.graphics.Color
+import com.rure.deepmedi.ui.theme.Black
+import com.rure.deepmedi.ui.theme.ChipAttention
+import com.rure.deepmedi.ui.theme.ChipDanger
+import com.rure.deepmedi.ui.theme.ChipHealthy
+import com.rure.deepmedi.ui.theme.ChipNormal
+import com.rure.deepmedi.ui.theme.ChipWarning
+import com.rure.deepmedi.ui.theme.White
+
+enum class Degree(
+    val tag: String,
+    val color: Color,
+    val textColor: Color
+) {
+    Danger("위험", ChipDanger, White),
+    Warning("경고", ChipWarning, Black),
+    Attention("주의", ChipAttention, Black),
+    Normal("정상", ChipNormal, Black),
+    Healthy("건강", ChipHealthy, White)
 }
